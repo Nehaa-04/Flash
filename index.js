4fc1904fa0acff6c6cf382b5cab8a300eda6e8e6
Nehaa-04 <nehabitodkar06@gmail.com>
2023-05-23 14:25:07 +0530

    Adding-html-and-js-file

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function onsignup(event) {
+    event.preventDefault();
+    console.log(event.target.username.value)
+    console.log(event.target.email.value);
+    console.log(event.target.phone.value);
+    console.log(event.target.date.value);
+    console.log(event.target.time.value);
+ }
